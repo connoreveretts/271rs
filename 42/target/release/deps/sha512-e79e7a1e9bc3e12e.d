464c89023196a3553b3fd122a816946ea26303b6
Connor <154204465+connoreveretts@users.noreply.github.com>
2025-10-13 13:09:28 -0700

    Add files via upload

---
+++ 42/target/release/deps/sha512-e79e7a1e9bc3e12e.d
@@ -0,0 +1,5 @@
+/Users/connoreveretts/Downloads/42/target/release/deps/sha512-e79e7a1e9bc3e12e.d: src/main.rs
+
+/Users/connoreveretts/Downloads/42/target/release/deps/sha512-e79e7a1e9bc3e12e: src/main.rs
+
+src/main.rs:
---
+++ 42/target/release/sha512.d
@@ -0,0 +1,1 @@
+/Users/connoreveretts/Downloads/42/target/release/sha512: /Users/connoreveretts/Downloads/42/src/main.rs
